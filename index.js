66014a0a2809948722ebdbdb66f0c6b3f3269a93
trose618 <trose618@gmail.com>
2019-04-24 14:33:31 -0400

    implemented insetion sort

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-function findMinAndRemove(array){
+function findMinAndRemove(array) {
+    let min = array[0]
+    let minIndex = 0
+    for (let i = 0; i < array.length; i++) {
+        let current = array[i];
+
+        if (current < min) {
+            min = current
+            minIndex = i
+        }
+    }
+
+    array.splice(minIndex, 1)
+    return min;
 
 }
 
-function insertionSort(array){
+function insertionSort(array) {
+    let sorted = []
 
+    while(array.length != 0){
+        let min = findMinAndRemove(array)
+        sorted.push(min)
+    }
+    return sorted;
 }
